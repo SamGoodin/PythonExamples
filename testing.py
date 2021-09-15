32de9c93c80608209babcc6d6124d286d8b8a57a
Sam Goodin <samuelgoodin@sbcglobal.net>
2021-09-15 15:29:29 -0400

    added pdfplumber testing and test document

--- testing.py
+++ testing.py
@@ -2,3 +2,12 @@ from glob import glob
 
 #Prints out all pdf files within this directory
 print(glob('*.pdf'))
+
+import pdfplumber
+
+pdf = pdfplumber.open('Capstone projects.pdf')
+#page = pdf.pages[0]
+for page in pdf.pages:
+    text = page.extract_text()
+    print(text)
+pdf.close()
